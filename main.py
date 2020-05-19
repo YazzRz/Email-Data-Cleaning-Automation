afa09fb69f8b0b4ba9f38e0bca2cf0d6d0ab6a7a
Arnulfo R <drarn96@gmail.com>
2020-05-18 23:27:13 -0500

    Using EmailMessage class for formatting and adding an image as attachment

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 import smtplib
+import imghdr
+from email.message import EmailMessage
 
 PYTHON_TEST_GMAIL_KEY = os.environ.get('PYTHON_TEST_GMAIL_KEY')
 EMAIL_MASTER = os.environ.get('EMAIL_MASTER')
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+msg = EmailMessage()
+msg['Subject'] = "Am I a business man?"
+msg['From'] = EMAIL_MASTER
+msg['To'] = EMAIL_MASTER
+msg.set_content('I am good talking about finance but my business skills though...')
 
+with open('businessman.png', 'rb') as f:
+    file_data = f.read()
+    file_name = f.name
+    file_type = imghdr.what(file_name)
+
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_MASTER, PYTHON_TEST_GMAIL_KEY)
-
-    subject = "Grab dinner this weekend?"
-    body = "How about dinner at 6pm this Saturday"
-
-    msg = f"Subject: {subject}\n\n{body}"
-
-    smtp.sendmail(EMAIL_MASTER, EMAIL_MASTER, msg)
+    smtp.send_message(msg)
